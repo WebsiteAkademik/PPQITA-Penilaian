be1dcf0c4bd341fc4896bc616d78973f4a3a7dc6
Gabriella Caroline PN <lalagabriella03@gmail.com>
2024-01-25 06:49:20 +0700

    perubahan login: mengedit tampilan login

--- resources/views/pages/user/login.blade.php
+++ resources/views/pages/user/login.blade.php
@@ -1,96 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Log in</title>
+@extends('layouts.base')
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+@push('style')
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+@endpush
 
-    <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
+@section('title')
+    Login
+@endsection
 
-    <link rel="stylesheet" href="{{asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
-
-    <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css?v=3.2.0')}}">
-    <script nonce="ee3a7274-8612-4c1f-afe6-b14b08931e3d">try { (function(w,d){!function(o,p,q,r){o[q]=o[q]||{};o[q].executed=[];o.zaraz={deferred:[],listeners:[]};o.zaraz.q=[];o.zaraz._f=function(s){return async function(){var t=Array.prototype.slice.call(arguments);o.zaraz.q.push({m:s,a:t})}};for(const u of["track","set","debug"])o.zaraz[u]=o.zaraz._f(u);o.zaraz.init=()=>{var v=p.getElementsByTagName(r)[0],w=p.createElement(r),x=p.getElementsByTagName("title")[0];x&&(o[q].t=p.getElementsByTagName("title")[0].text);o[q].x=Math.random();o[q].w=o.screen.width;o[q].h=o.screen.height;o[q].j=o.innerHeight;o[q].e=o.innerWidth;o[q].l=o.location.href;o[q].r=p.referrer;o[q].k=o.screen.colorDepth;o[q].n=p.characterSet;o[q].o=(new Date).getTimezoneOffset();if(o.dataLayer)for(const B of Object.entries(Object.entries(dataLayer).reduce(((C,D)=>({...C[1],...D[1]})),{})))zaraz.set(B[0],B[1],{scope:"page"});o[q].q=[];for(;o.zaraz.q.length;){const E=o.zaraz.q.shift();o[q].q.push(E)}w.defer=!0;for(const F of[localStorage,sessionStorage])Object.keys(F||{}).filter((H=>H.startsWith("_zaraz_"))).forEach((G=>{try{o[q]["z_"+G.slice(7)]=JSON.parse(F.getItem(G))}catch{o[q]["z_"+G.slice(7)]=F.getItem(G)}}));w.referrerPolicy="origin";w.src="/cdn-cgi/zaraz/s.js?z="+btoa(encodeURIComponent(JSON.stringify(o[q])));v.parentNode.insertBefore(w,v)};["complete","interactive"].includes(p.readyState)?zaraz.init():o.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,"zarazData","script");})(window,document) } catch (err) {
-        console.error('Failed to run Cloudflare Zaraz: ', err)
-        fetch('/cdn-cgi/zaraz/t', {
-            credentials: 'include',
-            keepalive: true,
-            method: 'GET',
-        })
-        };</script>
-</head>
-
-<body class="hold-transition login-page">
-    <div class="login-box">
-        <div class="login-logo">
-            <a href="#"><b>Admin</b>LTE</a>
-        </div>
-
-        <div class="card">
-            <div class="card-body login-card-body">
-                <p class="login-box-msg">Sign in to start your session</p>
-                
-                <form action="{{ route('postlogin') }}" method="post">
-                    {{ csrf_field() }}
-                    <div class="input-group mb-3">
-                    <input type="email" class="form-control" name="email" placeholder="Email" required>
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-envelope"></span>
-                            </div>
-                        </div>
+@section('content')
+    <div class="parent-form">
+        <main class="form-signin m-auto">
+            <form action="{{ route('postlogin') }}" method="POST">
+                @csrf
+                <img class="mb-4" src="{{ asset('assets/galeri/logo-transparent.png') }}" alt="" style="width: 25%">
+                <h1>LOGIN ADMIN</h1>
+                @if (session()->has('loginError'))
+                    <div class="alert alert-danger alert-dismissible fade show mb-2" role="alert">
+                        {{ session('loginError') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                     </div>
-                <div class="input-group mb-3">
-                    <input type="password" class="form-control" name="password" placeholder="Password" required>
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-lock"></span>
-                        </div>
-                    </div>
+                @endif
+
+                <div class="form-floating">
+                    <input type="email" name="email" class="form-control" id="floatingInput"
+                        placeholder="name@example.com">
+                    <label for="floatingInput">Email</label>
                 </div>
-                <div class="row">
-                    <div class="col-8">
-                        <div class="icheck-primary">
-                            <input type="checkbox" id="remember">
-                            <label for="remember">
-                            Remember Me
-                            </label>
-                        </div>
-                    </div>
-
-                    <div class="col-4">
-                    <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-                    </div>
-
+                <div class="form-floating mt-2">
+                    <input type="password" name="password" class="form-control" id="floatingPassword"
+                        placeholder="Password">
+                    <label for="floatingPassword">Password</label>
                 </div>
-                </form>
-            <div class="social-auth-links text-center mb-3">
-                <p>- OR -</p>
-                <a href="#" class="btn btn-block btn-primary">
-                    <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-                </a>
-                <a href="#" class="btn btn-block btn-danger">
-                    <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-                </a>
-            </div>
 
-            <p class="mb-1">
-                <a href="forgot-password.html">I forgot my password</a>
-            </p>
-            <p class="mb-0">
-                <a href="register.html" class="text-center">Register a new membership</a>
-            </p>
-            </div>
-        </div>
+                <div class="checkbox mb-4 mt-2">
+                    <label>
+                        <input type="checkbox" name="rememberme" value="rememberme"> Remember me
+                    </label>
+                </div>
+                <button class="w-100 btn btn-lg mb-3 text-white" style="background-color: var(--primary-color);"
+                    type="submit">Login</button>
+            </form>
+        </main>
     </div>
-
-
-    <script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
-
-    <script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-
-    <script src="{{asset('dist/js/adminlte.min.js?v=3.2.0')}}"></script>
-</body>
-</html>
+@endsection
