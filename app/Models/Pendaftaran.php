d0a3358f455061939d3da3dab6da1686d82101a0
sukmalarasati <89761105+sukmalarasati@users.noreply.github.com>
2024-01-24 20:55:13 +0700

    form pendaftaran: pendaftaran model

---
+++ app/Models/Pendaftaran.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pendaftaran extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        // 'user_id',
+        'no_nisn',
+        'nama_calon_siswa',
+        'tempat_lahir',
+        'tanggal_lahir',
+        'tinggi_badan',
+        'no_kartu_keluarga',
+        'no_wa_anak',
+        'berat_badan',
+        'alamat_rumah',
+        'penyakit_kronis',
+        'kelurahan',
+        'dukuh',
+        'kabupaten',
+        'kecamatan',
+        'asal_sekolah',
+        'kodepos',
+        'nama_ayah',
+        'ayah_hidup',
+        'ibu_hidup',
+        'pekerjaan_ayah',
+        'pekerjaan_ibu',
+        'nama_ibu',
+        'informasi_pmb',
+        'no_telepon_ortu',
+    ];
+}
