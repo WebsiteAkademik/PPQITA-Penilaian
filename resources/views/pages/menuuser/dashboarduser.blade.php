3427c1f20521d54b85cb310a79ce119f496a3015
sukmalarasati <89761105+sukmalarasati@users.noreply.github.com>
2024-02-03 01:06:01 +0700

    menambahkan button download di dashboarduser

--- resources/views/pages/menuuser/dashboarduser.blade.php
+++ resources/views/pages/menuuser/dashboarduser.blade.php
@@ -13,7 +13,7 @@
             <div class="card w-100">
                 <div class="card-body p-4">
                     <center><h2 class="card-title fw-semibold mb-4">SELAMAT ANDA TELAH BERHASIL MELAKUKAN PENDAFTARAN
-                        PENERIMAAN PESERTA DIDIK BARU <br>PPQIT AL MAHIR SUrakarta<br>TAHUN AJARAN 2024 / 2025
+                        PENERIMAAN PESERTA DIDIK BARU <br>PPQIT AL MAHIR Surakarta<br>TAHUN AJARAN 2024 / 2025
                     </h2></center>
                     <h3 class="card-title fw-semibold mb-4">Ketentuan Yang Dilakukan Setelah Melaksanakan Pendaftaran Adalah Sebagai Berikut :</h3>
                     
@@ -44,76 +44,11 @@
                         </ol>
                     <b class="fw-semibold mb-4">Konfirmasi Pembayaran Ke : LINK (<a href="https://wa.me/+6281239616477">Share on WhatsApp</a>)</b>
 
-                    <div class="table-responsive">
-                        <table class="table text-nowrap mb-0 align-middle">
-                            <thead class="text-dark fs-4">
-                                <tr>
-                                
-                                    <th class="border-bottom-0">
-                                        <h6 class="fw-semibold mb-0">No NISN & Nama Calon Siswa</h6>
-                                    </th>
-                                    <th class="border-bottom-0">
-                                        <h6 class="fw-semibold mb-0">No Telp</h6>
-                                    </th>
-                                    <th class="border-bottom-0">
-                                        <h6 class="fw-semibold mb-0">Status</h6>
-                                    </th>
-                                    
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($pendaftars as $key => $pendaftar)
-                            
-                                <a href="{{ route('cetak_pdf', $pendaftar->no_nisn) }}"
-                                    class="text-black text-center d-flex align-items-center justify-content-center  bg-warning"
-                                    style="padding:10px;border:solid 2px #000000"> 
-                                    <i class="fa-solid fa-print"></i> &nbsp; &nbsp;
-                                    <strong>CETAK FORMULIR</strong>
-                                </a>
-                                <br><br>
-                                    <tr>
-                                        <td class="border-bottom-0">
-                                            <h2 class="fw-semibold mb-1">{{ $pendaftar->no_nisn }}</h2>
-                                            <h4 class="fw-normal mb-1">{{ $pendaftar->nama_calon_siswa }}</h4>
-                                            <br/><span class="fw-normal mb-1">{{ "Nomor Telepon : " . $pendaftar->no_telepon }}</span>
-                                            <br/><span class="fw-normal mb-1">{{ "Tempat Lahir : " . $pendaftar->tempat_lahir }}</span>
-                                            <br/><span class="fw-normal mb-1">{{ "Tanggal Lahir : " . $pendaftar->tanggal_lahir }}</span>
-                                            <br/><span class="fw-normal mb-1">{{ "No KK : " . $pendaftar->no_kartu_keluarga }}</span>
-                                            <br/><span class="fw-normal mb-1">{{ "Agama : " . $pendaftar->agama }}</span>
-                                            <br/><span class="fw-normal mb-1">{{ "Alamat Rumah : " . $pendaftar->alamat_rumah }}</span>
-                                            <br/><span class="fw-normal mb-1">{{ "Nama Ayah : " . $pendaftar->nama_ayah }}</span>
-                                      
-                                        </td>
-                                        <td class="border-bottom-0">
-                                            <p class="mb-0 fw-normal">{{ $pendaftar->no_telepon }}</p>
-                                        </td>
-                                        <td class="border-bottom-0">
-                                            @php
-                                                switch ($pendaftar->status) {
-                                                    case 'open':
-                                                        echo "<div class='badge text-capitalize' style='background-color: #00569C;'>$pendaftar->status</div>";
-                                                        break;
-                                                    case 'testing':
-                                                        echo "<div class='badge text-capitalize bg-info'>$pendaftar->status</div>";
-                                                        break;
-                                                    case 'wawancara':
-                                                        echo "<div class='badge text-capitalize bg-warning'>$pendaftar->status</div>";
-                                                        break;
-                                                    case 'diterima':
-                                                        echo "<div class='badge text-capitalize bg-success'>$pendaftar->status</div>";
-                                                        break;
-                                                    case 'tidak diterima':
-                                                        echo "<div class='badge text-capitalize bg-danger'>$pendaftar->status</div>";
-                                                        break;
-                                                }
-                                            @endphp
-                                        </td>
-                                        
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
+                    <div class="row mt-4">
+                            <div class="col-12 col-lg-5">
+                                <button type="submit" class="btn btn-success" style="padding:10px;box-shadow:5px 5px 10px">Download</button>
+                            </div>
+                        </div>
                 </div>
             </div>
         </div>
