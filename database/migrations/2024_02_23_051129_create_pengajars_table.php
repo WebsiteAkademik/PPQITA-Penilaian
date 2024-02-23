99450c8b6ceeea1f8e92c91470939cf1085091c1
Gabriella Caroline PN <lalagabriella03@gmail.com>
2024-02-23 12:17:15 +0700

    migration dan model Pengajar

---
+++ database/migrations/2024_02_23_051129_create_pengajars_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengajars', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_pengajar');
+            $table->string('password');
+            $table->string('alamat', 100);
+            $table->string('no_wa_pengajar', 13);
+            $table->string('mapel_ampu');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengajars');
+    }
+};
