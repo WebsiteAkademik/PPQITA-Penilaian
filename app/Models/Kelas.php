591a845a20c99487ff2cba546216fcfa57466c7c
Gabriella Caroline PN <lalagabriella03@gmail.com>
2024-02-24 10:22:21 +0700

    fixing table DetailSetupMataPelajaran

---
+++ app/Models/Kelas.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Kelas extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['kelas', 'mata_pelajaran_id'];
+
+    public function kelas()
+    {
+        return $this->belongsTo(MataPelajaran::class);
+    }
+}
