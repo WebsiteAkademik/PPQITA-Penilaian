86a802921e8969051bb767750a929f1b40ca5308
sukmalarasati <89761105+sukmalarasati@users.noreply.github.com>
2024-03-03 14:22:15 +0700

    Membuat Cetak PDF Rapor UTS & UAS

---
+++ resources/views/pages/admin/akademik/rapor/cetakraporuts.blade.php
@@ -0,0 +1,419 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>RAPOR UTS</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+	<?php
+    date_default_timezone_set('Asia/Jakarta'); //Sesuai zona waktu
+	?>
+    
+	<style type="text/css">
+        .content {
+            width: 85%;
+            margin: 0 auto;
+        }
+
+        table {
+            width: 100%;
+        }
+
+        table * {
+            border-collapse: collapse;
+            border: 1px solid black;
+        }
+
+		table tr td,
+		table tr th{
+			font-size: 14px;
+		}
+
+        .page-break {
+            page-break-after: always;
+        }
+
+        .noborder * {
+            border: none !important;
+        }
+	</style>
+
+			<center><img src="assets/galeri/kop-rapor.png" width="100%"/></center> <br/><br/>
+	@php setlocale(LC_TIME, 'id_ID'); @endphp
+	@php $i=1 @endphp
+	<center>
+		<h6>LAPORAN PENILAIAN HASIL UTS SANTRI</h6>
+		<h6>TAHUN PELAJARAN 2023/2024</h6>
+	</center>
+    <div class="content">
+        <table class="noborder">
+            <tbody>
+                <tr>
+                    <th style="width: 100px;">Nama</th>
+                    <th style="width: 10px;">:</th>
+                    <th style="width: 300px;">asdf</th>
+                    <th style="width: 100px;">Kelas</th>
+                    <th style="width: 10px;">:</th>
+                    <th>asdf</th>
+                </tr>
+                <tr>
+                    <th>Nomor Induk</th>
+                    <th>:</th>
+                    <th>asdf</th>
+                    <th>Semester</th>
+                    <th>:</th>
+                    <th>asdf</th>
+                </tr>
+            </tbody>
+        </table>
+        <table>
+            <thead>
+                <tr>
+                    <th style="text-align: center; width: 30px;">
+                        No
+                    </th>
+                    <th style="text-align: center;">
+                        Mata Pelajaran
+                    </th>
+                    <th style="text-align: center; width: 75px;">
+                        KKM
+                    </th>
+                    <th style="text-align: center; width: 75px;">
+                        Nilai
+                    </th>
+                    <th style="text-align: center; width: 75px;">
+                        Rata-Rata Kelas
+                    </th>
+                    <th style="text-align: center; width: 85px;">
+                        Deskripsi Kemajuan Belajar
+                    </th>
+                </tr>
+            </thead>
+            <thead>
+                <tr>
+                    <th colspan="6">0. ASDF</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        1
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        2
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        3
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                </tr>
+            </tbody>
+            <thead>
+                <tr>
+                    <th colspan="6">0. ASDF</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        1
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        2
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        3
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                </tr>
+            </tbody>
+            <thead>
+                <tr>
+                    <th colspan="6">0. ASDF</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        1
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        2
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        3
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                </tr>
+            </tbody>
+            <tbody>
+                <tr>
+                    <td>
+                        
+                    </td>
+                    <td colspan="2">
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                    <td colspan="2" rowspan="2">
+                        asdf
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        
+                    </td>
+                    <td colspan="2">
+                        asdf
+                    </td>
+                    <td>
+                        asdf
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="page-break"></div>
+    <center><img src="assets/galeri/kop-rapor.png" width="100%"/></center> <br/><br/>
+    <div class="content">
+        <h6>Pengembangan diri bla bla bla</h6>
+        <table>
+            <thead>
+                <tr>
+                    <th style="text-align: center; width: 75px;">No</th>
+                    <th style="text-align: center; width: 75px;">Nama</th>
+                    <th style="text-align: center; width: 75px;">Nilai</th>
+                    <th style="text-align: center; width: 75px;">Keterangan</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>asdf</td>
+                    <td>asdf</td>
+                    <td>asdf</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>asdf</td>
+                    <td>asdf</td>
+                    <td>asdf</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>asdf</td>
+                    <td>asdf</td>
+                    <td>asdf</td>
+                </tr>
+            </tbody>
+        </table>
+        <br/>
+        <div style="width: 45%; float: left;">
+            <h6>Ketidakhadiran</h6>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Sakit</th>
+                        <td>1</td>
+                    </tr>
+                    <tr>
+                        <th>Izin</th>
+                        <td>1</td>
+                    </tr>
+                    <tr>
+                        <th>Tanpa Keterangan</th>
+                        <td>1</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div style="width: 10%; float: left;">
+        </div>
+        <div style="width: 45%; float: left;">
+            <h6>Saran / Nasihat</h6>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>
+                            Alhamdulilah, nilai Ananda sangat baik. Semoga selalu semangat bersekolah.
+                        </th>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div style="width: 100%; clear: both;"></div>
+        <br/>
+        <br/>
+        <table class="noborder">
+            <tbody>
+                <tr>
+                    <td colspan="5" style="text-align: right;">Jakarta, 03 Maret 2024</td>
+                </tr>
+                <tr>
+                    <td style="text-align: center;">Orang Tua / Wali</td>
+                    <td style="width: 10%;"></td>
+                    <td style="text-align: center;">Mengetahui<br/>Kepala Sekolah</td>
+                    <td style="width: 10%;"></td>
+                    <td style="text-align: center;">Wali Kelas</td>
+                </tr>
+                <tr>
+                    <td colspan="5" style="text-align: center;"><br/><br/><br/><br/><br/></td>
+                </tr>
+                <tr>
+                    <td style="text-align: center;">(&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)</td>
+                    <td style="width: 10%;"></td>
+                    <td style="text-align: center;">Siapa ya</td>
+                    <td style="width: 10%;"></td>
+                    <td style="text-align: center;">Siapa ya</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
